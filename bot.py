cdfc975c108b5ed3219fd6512ad9e65569055742
Pulkit Munjral <44071448+pulkitmunjral@users.noreply.github.com>
2022-09-23 12:40:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,48 +1,54 @@
 from flask import Flask, request
 import telebot
 import os
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+button1 = InlineKeyboardButton(text="Random 1-10", callback_data="randomvalue_of10")
+button2 = InlineKeyboardButton(text="Random 1-100", callback_data="randomvalue_of100")
+
+keyboard_inline = InlineKeyboardMarkup().add(button1, button2)
 
 TOKEN = "5631307589:AAHorsEa3ZlCcAOKmdWKPCB0UWIFYrjMhbk"
-bot = telebot.TeleBot(token=TOKEN)
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+# bot = telebot.TeleBot(token=TOKEN)
 server = Flask(__name__)
 
-def findat(msg):
-    # from a list of texts, it finds the one with the '@' sign
-    for i in msg:
-        if '@' in i:
-            return i
 
-@bot.message_handler(commands=['start']) # welcome message handler
-def send_welcome(message):
-    bot.reply_to(message, '(placeholder text)')
+# @bot.message_handler(commands=['start'])
+# def send_welcome(message):
+#     bot.reply_to(message, "Hi Buddy, How can i help you")
+#
+# @bot.message_handler(commands=['help']) # help message handler
+# def send_welcome(message):
+#     bot.reply_to(message, 'ALPHA = FEATURES MAY NOT WORK')
 
-@bot.message_handler(commands=['help']) # help message handler
-def send_welcome(message):
-    bot.reply_to(message, 'ALPHA = FEATURES MAY NOT WORK')
-
-@bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
-# lambda function finds messages with the '@' sign in them
-# in case msg.text doesn't exist, the handler doesn't process it
-def at_converter(message):
-    texts = message.text.split()
-    at_text = findat(texts)
-    if at_text == '@': # in case it's just the '@', skip
-        pass
-    else:
-        insta_link = "https://instagram.com/{}".format(at_text[1:])
-        bot.reply_to(message, insta_link)
+@dp.message_handlers(commands=["start", "help"])
+async def welcome(message: types.Message):
+    await message.reply("Hi hello", reply_markup=keyboard_inline)
 
 
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+@dp.callback_query_handler(text=["randomvalue_of10", "randomvalue_of10"])
+async def random(call: types.CallbackQuery):
+    if call.data == "randomvalue_of10":
+        await call.message.answer("10")
+    elif call.data == "randomvalue_of100":
+        await call.message.answer("100")
+
+
+#
+# @server.route('/' + TOKEN, methods=['POST'])
+# def getMessage():
+#     dp.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+#     return "!", 200
 
 @server.route("/")
 def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://myapplicationtelegrambot.herokuapp.com/' + TOKEN)
+    await bot.set_webhook(url='https://myapplicationtelegrambot.herokuapp.com/' + TOKEN)
     return "!", 200
+
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
