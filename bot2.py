03ed738eda7a306777b0ac2a5222068312089fcd
b0281269 <pulkit.munjral@airtel.com>
2022-09-23 13:58:31 +0530

    second shift

--- bot2.py
+++ bot2.py
@@ -26,9 +26,9 @@ async def welcome(message: types.Message):
 
 @dp.callback_query_handler(text=["env"])
 async def env(call: types.CallbackQuery):
-    env = {"telemedia_env": ["PT", "SIT"]}
+    env_dict = {"telemedia_env": ["PT", "SIT"]}
     call_back_function = "random"
-    keyboard_inline = make_keyboard(env[str(call.message) + "_env"], call_back_function)
+    keyboard_inline = make_keyboard(env_dict[str(call.message) + "_env"], call_back_function)
     await call.message.answer("please choice the env", reply_markup=keyboard_inline)
 
 
