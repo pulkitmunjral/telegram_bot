18e602b4c7f3fafd96ac831329360da45e8b93d3
Pulkit Munjral <44071448+pulkitmunjral@users.noreply.github.com>
2022-09-23 12:46:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ server = Flask(__name__)
 # def send_welcome(message):
 #     bot.reply_to(message, 'ALPHA = FEATURES MAY NOT WORK')
 
-@dp.message_handlers(commands=["start", "help"])
+@dp.message_handler(commands=["start", "help"])
 async def welcome(message: types.Message):
     await message.reply("Hi hello", reply_markup=keyboard_inline)
 
